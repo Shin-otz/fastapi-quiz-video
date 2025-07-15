b121887af4041c7f4edb2fb4ba402f1eeeebe937
Shin-otz <sswotz@gmail.com>
2025-07-15 07:33:45 +0700

    Test...

--- main.py
+++ main.py
@@ -318,9 +318,9 @@ def make_quiz_video_with_title_top(data_, output_path):
     final_audio.write_audiofile(output_audio_path)
 
     try:
-        image_input = ffmpeg.input(bgimage_path, loop=1, t=total_duration).filter('scale', 1080, 720)
+        image_input = ffmpeg.input(bgimage_path, loop=1, t=total_duration)
         audio_input = ffmpeg.input(output_audio_path)
-
+        base = image_input.filter('scale', 1080, 720)
 
         # 제목
         video = base.drawtext(
